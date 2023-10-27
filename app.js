40ea0d2aad1a4182f8e1c60945d7e84162c3bb07
Yahya saad <yahyasaad.me@gmail.com>
2023-10-27 21:34:08 +0530

    I

--- app.js
+++ app.js
@@ -1,32 +1,16 @@
 const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
 const path = require("path")
 const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
 
 // Serve a basic HTML page
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-// Set up Socket.IO event handlers
-io.on('connection', (socket) => {
-  console.log('A user connected');
-
-  // Handle custom events
-  socket.on('message', (data) => {
-    console.log('Message received:', data);
-    io.emit('message', data); // Broadcast the message to all clients
-  });
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
-});
-
+app.get("/",(req,res)=>{
+  res.send("Welcome")
+})
 // Start the server
-server.listen(5000, () => {
+app.listen(5000, () => {
   console.log('Server listening on port 3000');
 });
