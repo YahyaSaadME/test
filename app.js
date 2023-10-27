4345ab8d31c9822161bde03ea7595509963eba38
Yahya saad <yahyasaad.me@gmail.com>
2023-10-27 21:44:15 +0530

    I

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.get("/",(req,res)=>{
+app.get("/w",(req,res)=>{
   res.send("Welcome")
 })
 // Start the server
